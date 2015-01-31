38b108221dc0c9f739474775a0b0825ae007e9a7
Jewl Johnson <jewljohnson43@gmail.com>
2015-01-30 21:43:04 -0500

    Added a few lines of code

--- Test-For-Git/src/Test.java
+++ Test-For-Git/src/Test.java
@@ -4,5 +4,8 @@ public class Test
 	public static void main( String args[] )
 	{
 		System.out.println(" Here is a new java project ");
+		System.out.println(" Adding a few more lines of code");
+		System.out.println(" Adding a few more lines of code");
+		System.out.println(" Adding a few more lines of code");
 	}
 }
